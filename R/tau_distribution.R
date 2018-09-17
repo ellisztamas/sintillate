95fca1a12d9bff11ab8ad7f1008ba179c91e30c5
Tom Ellis <thomas.ellis@ebc.uu.se>
2018-09-17 17:04:19 +0200

    Added hist_vals.R as a general fucntion to cut up a vector into bins, and count the frequency of each. hist_vals doesn't depend on that vector being tau values. Altered tau_distribution to run hist_vals on observed data and a matrix of bootstrapped tau vals. Added tests for tau_distribution

--- R/tau_distribution.R
+++ R/tau_distribution.R
@@ -1,37 +1,57 @@
 #' Distribution of pleiotropic effects
 #'
-#' Bins values of \eqn{\tau} across pairs of observations so that a histogram can be created.
+#' Bins values of \eqn{\tau} across pairs of observations so that a histogram
+#' can be created.
 #'
 #' \code{tau_distribution} groups values of \eqn{tau} over observations into bins
 #' defined by \code{width}. Within each bin, values for \emph{q} for each
-#' \eqn{\tau} are summed. The resulting sum can be interpreted as the
-#' probability of observing a vector with \eqn{\tau} in that bin, accoutning for
-#' the robustness of the estimate of \eqn{\tau}.
+#' \eqn{\tau} are summed. In addition, a matrix of \eqn{tau} values can be given to estimate confidence
+#' intervals within each bin.
 #'
 #' @param tau Vector of \eqn{\tau} values.
-#' @param tau Vector of \eqn{q} values.
-#' @param width Width of each bin. Passed to \code{cut}. Since \eqn{\tau} can
+#' @param bootstrap Optional matrix of \eqn{tau} values derived from bootstrapping
+#' the *x* and *y* values used to create vector \code{tau}. This should have a row
+#' for every element in \code{tau}.
+#' @param width Width of each bin passed to \code{cut}. Since \eqn{\tau} can
 #' only be between -1 and 1, it is meaningless to assign a width greater than
 #' that interval.
 #'
-#' @return Data.frame listing bining levels, the midpoints for each bin, and the
-#' sums of \emph{q}-values in each bin.
+#' @return Data.frame listing midpoints for each bin, and density within each
+#' bin. If bootstrapped values of \eqn{tau} are provided, upper and lower
+#' confidence intervals are returned as well.
 #'
 #' @export
-tau_distribution <- function(tau, q, width = 1/8){
+tau_distribution <- function(x, bootstrap=NULL, width = 1/8){
   if(width > 2){
     stop("Width > 2. Since $\\tau$ can only be between -1 and 1, it is meaningless to assign a width greater than that interval.")
   }
-  if(length(q) != length(tau)){
-    stop("Length of q and tau do not match.")
-  }
+  # Check input value all fall between -1 and 1.
+  if(any(x >  1)) stop("One or more values of tau are greater than 1.")
+  if(any(x < -1)) stop("One or more values of tau are less than 1.")
+
   breaks <- seq(-1, 1, width)
-  bks    <- cut(tau, breaks)
+  yvals <- as.numeric(hist_vals(x, breaks))
 
-  yvals <- tapply(q, bks, sum, na.rm=TRUE)
-  yvals[is.na(yvals)] <- 0
-  yvals <- yvals / sum(yvals)
+  if(is.null(bootstrap)){
+    out <- data.frame(
+      midpoint = breaks[-1]-width/2,
+      density  = yvals)
+  } else {
+    if(nrow(bootstrap) != length(x)){
+      stop("Matrix of bootstrap values has", nrow(bootstrap), "rows, but the original dataset has", nrow(x))
+    }
+    # Check bootstrap values all fall between -1 and 1.
+    if(any(bootstrap >  1)) stop("One or more values in bootstrap are greater than 1.")
+    if(any(bootstrap < -1)) stop("One or more values in bootstrap are less than 1.")
 
-  data.frame(interval = levels(bks), midpoint = breaks[-1]-width/2, density=yvals, row.names = NULL)
+    boot_cut <- apply(bootstrap, 2, hist_vals, breaks=breaks)
+    out <- data.frame(
+      midpoint = breaks[-1]-width/2,
+      density  = yvals,
+      lower_CI = apply(boot_cut, 1, quantile, 0.025),
+      upper_CI = apply(boot_cut, 1, quantile, 0.975),
+      row.names = NULL
+    )
+  }
+  return(out)
 }
-

a4021fdab76b25413ec46086371dd0a5519fb0d7
Ellis <thomas.ellis@pcm-gmi-55.gmi.oeaw.ac.at>
2019-02-04 15:25:19 +0100

    Added function parametric_resample.R for efficient parametric boostrapping of a single parameter.

---
+++ R/parametric_resample.R
@@ -0,0 +1,19 @@
+#' Gaussian parametric bootstrapping
+#'
+#' Generate a sample of parametric bootstrap samples for a vector of values with
+#' associated standard errors.
+#'
+#' @param x Vector of parameter estimates.
+#' @param se Vector of standard errors for each element in `x`.
+#' @param R Number of samples to draw.
+#'
+#' @return A matrix of bootstrap samples with a row for each element in `x` and
+#' `R` columns.
+#' @export
+#' @author Tom Ellis
+parametric_resample <- function(x, se, R){
+  vals <- rnorm(R*length(x))
+  vals <- matrix(vals, nrow=length(x))
+  vals <- (vals * se) + x
+  return(vals)
+}

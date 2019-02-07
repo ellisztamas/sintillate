b7511df65c9a3e9d767f8e1f929c59186bf4b6bb
Ellis <thomas.ellis@pcm-gmi-55.gmi.oeaw.ac.at>
2019-02-07 15:10:54 +0100

    Altered plot_tau_dist.R so that points are plotted over the bars, and to remove passing additional variables to points() (i.e. only to plot()).

--- R/plot_tau_dist.R
+++ R/plot_tau_dist.R
@@ -9,7 +9,7 @@
 #' \code{?sintillate} for more.
 #' @param width Width of the bars. Defaults to 1/8.
 #' @param length Length of the horizontal ends of the error bars.
-#' @param ... Further arguments passed to barplot and points.
+#' @param ... Further arguments passed to barplot.
 #'
 #' @export
 plot_tau_dist <- function(tau, bootstraps, width=1/8, length=0.05, ...){
@@ -18,11 +18,12 @@ plot_tau_dist <- function(tau, bootstraps, width=1/8, length=0.05, ...){
   # Plot bars heights
   barplot(td$density, space = 0, ylim=c(0, max(td$upper_CI)), ...)
   axis(1, at = 0:4 * (nrow(td)/4), labels =seq(-1, 1, 1/2))
-  points(1:nrow(td)-0.5, td$mean, ...)
 
-  # add error bars
+  # add error bars and points
   arrows(1:nrow(td)-0.5, y0 = td$upper_CI,
          1:nrow(td)-0.5, y1 = td$lower_CI,
          angle = 90, code = 3, length = length
   )
+  points(1:nrow(td)-0.5, td$mean, col='black', type='o', pch=21, bg='white')
+
 }

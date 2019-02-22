ce6665837ecbc08b684f40517eeff21e8fa3163f
Tom Ellis <thomas.ellis@gmi.oeaw.ac.at>
2019-02-22 07:59:55 +0100

    Important change to how angles are calculated by angle360.R They now use the positive x-axis as the reference point and measure anticlockwise from there. Updated version number to reflect this to 0.2.2. Also: altered LICENSE to give my full name and GMI and email address.

--- R/angle360.R
+++ R/angle360.R
@@ -13,9 +13,10 @@ angle360 <- function(x, y){
   quadrant[x  < 0 & y  < 0] <- 3
   quadrant[x >= 0 & y  < 0] <- 4
   # calculate angles
-  theta <- atan(abs(x)/abs(y)) # for absolute x and y values
-  theta[quadrant == 2] <- pi*2 - theta[quadrant == 2] # correction for quadrant 2
+  theta <- atan(abs(y)/abs(x)) # for absolute x and y values
+  theta[quadrant == 2] <- pi/2 + theta[quadrant == 2] # correction for quadrant 2
   theta[quadrant == 3] <- pi   + theta[quadrant == 3] # correction for quadrant 3
-  theta[quadrant == 4] <- pi   - theta[quadrant == 4] # correction for quadrant 4
+  theta[quadrant == 4] <- 2*pi - theta[quadrant == 4] # correction for quadrant 4
+
   return(theta)
 }
